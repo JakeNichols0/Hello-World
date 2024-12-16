9e3d672e2f7962606957181d1db46c289ed88372
JakeNichols0 <adages.shows_03@icloud.com>
2024-12-16 14:11:02 -0600

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,18 +1,22 @@
 from time import sleep as wait
 import random
+import math
 
 def helloWorld(s):
     if(s == "p""r""i""n""t"):
         w = "H""e""l""l""o"" ""W""or""l""d""!"
     else:
         w = s
-    j = ""
+    j = 0
     for i in w:
         char = 0
-        while (char != ord(i)):
+        while (math.sqrt(char) != math.sqrt(ord(i))):
             char = random.randint(32, 122)
+            j += char
             wait(char/10000)
         print(chr(char), end="")
+        j /= char
+    print("\n", char(math.floor(j)))
 
 helloWorld("print")
 

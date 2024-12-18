8561724ad6af83008c51a4265fa8a8600d5ebb9b
Jake Nichols <adages.shows_03@icloud.com>
2024-12-17 18:41:58 -0600

    Prints in Ascii Art

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,11 @@
 from time import sleep as wait
 import random
 import math
+import pyfiglet
+
+def clear(pre):
+    for i in range(pre.count("\n")+1):
+        print("\033[1A""\x1b[2K", end="")
 
 def helloWorld(s):
     if(s == "p""r""i""n""t"):
@@ -8,13 +13,18 @@ def helloWorld(s):
     else:
         w = s
     j = 0
+    Scheiße = ""
+    Würstchen = ""
     for i in w:
         char = 0
         while (math.sqrt(char) != math.sqrt(ord(i))):
             char = random.randint(32, 122)
             j += char
             wait(char/10000)
-        print(chr(char), end="")
+        clear(Würstchen)
+        Scheiße += chr(char)
+        Würstchen = pyfiglet.figlet_format(Scheiße)
+        print(Würstchen)
         j /= char
     print("\n", char(math.floor(j)))
 
